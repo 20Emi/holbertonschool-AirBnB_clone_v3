665fc031d25edbfd986627668ddd26e5c2df74bd
20Emi <iop.emilysanchez1@gmail.com>
2023-08-23 13:27:36 -0700

    is added count() method from storage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,18 @@
 from flask import jsonify
 from api.v1.views import app_views
 
-
 @app_views.route('/status')
 def app_vi():
+    """returns a JSON: "status": "OK"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def app_vi1():
+    """is added count() method from storage"""
+    from models import storage
+    return jsonify({"amenities": storage.count("Amenities"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Reviews"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
