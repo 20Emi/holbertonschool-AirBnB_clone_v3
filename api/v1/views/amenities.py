a0d4be913ddf84bbeaa66a028931643aaad88c04
20Emi <iop.emilysanchez1@gmail.com>
2023-08-24 05:59:06 -0700

    task 9 incomplete

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+""""""
+
+from flask import Flask, jsonify, abort, request
+from models import storage
+from api.v1.views import app_views
+from models.amenity import Amenity
+
+
+@app_views.route('/amenities', methods=['GET'])
+def get_amenities():
+    st_amenities = storage.all(Amenity)
+    amenity_list = []
+    for obj in st_amenities:
+        amenity_list.append(obj.to_dict())
+    return jsonify(st_amenities)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+def get_amenities_id(amenity_id):
+    st_amenities = storage.get(Amenity, amenity_id)
+    if st_amenities is not None:
+        return jsonify(st_amenities.to_dict())
+    return abort(404)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+def detele_amenity(amenity_id):
+    st_amenities = storage.get(Amenity, amenity_id)
+    if st_amenities is None:
+        abort(404)
+    storage.delete(st_amenities)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['POST'])
+def post_amenity():
+    data = request.get_json()
+    if not data:
+        error_message = 'Not a JSON'
+        return jsonify(error_message), 400
+    if 'name' not in data:
+        error_message2 = 'Missing name'
+        return jsonify(error_message2), 400
+    amenity = storage()
+    amenity.name = data['name']
+    storage.save()
+    return jsonify(amenity.to_dict()), 201
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+def put_amenity(amenity_id):
+    data = request.get_json()
+    st_amenities = storage.get(Amenity, amenity_id)
+    if st_amenities is None:
+        abort(404)
+    if not data:
+        error_message = 'Not a JSON'
+        return jsonify(error_message), 400
+    st_amenities.name = data['name']
+    storage.save()
+    return jsonify(st_amenities.to_dict()), 200
