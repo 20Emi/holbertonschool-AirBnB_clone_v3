3e942925aed05693e69df6b556a9077af5aa673f
vicenzoanza <viceanza2010@hotmail.com>
2023-08-26 13:01:13 -0300

    task 8

---
+++ api/v1/views/cities.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+""""""
+
+from flask import Flask, jsonify, abort, request
+from models import storage
+from api.v1.views import app_views
+from models.city import City
+
+
+@app_views.route('states/<state_id>/cities', methods=['GET'])
+def all_cities_state(state_id):
+    state = storage.get(State, state_id)
+    
+    if not state:
+        abort(404)
+    
+    results = []
+    for city in storage.all("City").values():
+        if city.state_id == state_id:
+            results.append(city.to_dict())
+    return jsonify(results)
+
+
+@app_views.route('cities/<city_id>', methods=['GET'])
+def only_cities(city_id):
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    
+    results= []
+    results = city.to_dict()
+    return jsonify(results)
+
+
+@app_views.route('cities/<city_id>', methods=['DELETE'])
+def delete_city(city_id):
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    
+    result = {}
+    storage.delete(city)
+    storage.save()
+    return jsonify(result), 204
+
+@app_views.route('states/<state_id>/cities', methods=['POST'])
+def create_city(state_id):
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    
+    try:
+        json_get = request.get_json()
+    except:
+        abort(400, "Not a JSON")
+    
+    if 'name' not in json_get:
+        abort(400, "Missing name")
+    
+    new_city = City(name=json_get['name'], state_id=state.id)
+    storage.new(new_city)
+    storage.save()
+    
+    return jsonify(new_city.to_dict()), 201
+    
+@app_views.route('cities/<city_id>', methods=['PUT'])
+def update_city(city_id):
+    city= storage.get(City, city_id)
+    if not city:
+        abort(404)
+    
+    try:
+        json_get = request.get_json()
+    except:
+        abort(400, "Not a JSON")
+        
+    if json_get and isinstance(json_get, dict):
+        ignore = ['id', 'state_id', 'created_at', 'updated_at']
+        for key, value in json_get.items():
+            if key not in ignore:
+                setattr(city, key, value)
+        
+        storage.save()
+    
+    return jsonify(city.to_dict()), 200
+    
+        
+    
