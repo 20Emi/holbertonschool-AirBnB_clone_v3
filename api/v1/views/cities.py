443e14165de1f10c47b0cc5fd3bc775b08e50655
vicenzoanza <viceanza2010@hotmail.com>
2023-08-26 13:06:58 -0300

    fix

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,11 +9,9 @@ from models.city import City
 
 @app_views.route('states/<state_id>/cities', methods=['GET'])
 def all_cities_state(state_id):
-    state = storage.get(State, state_id)
-    
+    state = storage.get(State, state_id)   
     if not state:
         abort(404)
-    
     results = []
     for city in storage.all("City").values():
         if city.state_id == state_id:
@@ -26,7 +24,6 @@ def only_cities(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
-    
     results= []
     results = city.to_dict()
     return jsonify(results)
@@ -37,7 +34,6 @@ def delete_city(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
-    
     result = {}
     storage.delete(city)
     storage.save()
@@ -48,41 +44,31 @@ def create_city(state_id):
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    
     try:
         json_get = request.get_json()
     except:
         abort(400, "Not a JSON")
-    
     if 'name' not in json_get:
         abort(400, "Missing name")
-    
     new_city = City(name=json_get['name'], state_id=state.id)
     storage.new(new_city)
     storage.save()
-    
     return jsonify(new_city.to_dict()), 201
-    
+
+
 @app_views.route('cities/<city_id>', methods=['PUT'])
 def update_city(city_id):
     city= storage.get(City, city_id)
     if not city:
-        abort(404)
-    
+        abort(404)    
     try:
         json_get = request.get_json()
     except:
         abort(400, "Not a JSON")
-        
     if json_get and isinstance(json_get, dict):
         ignore = ['id', 'state_id', 'created_at', 'updated_at']
         for key, value in json_get.items():
             if key not in ignore:
                 setattr(city, key, value)
-        
         storage.save()
-    
     return jsonify(city.to_dict()), 200
-    
-        
-    
