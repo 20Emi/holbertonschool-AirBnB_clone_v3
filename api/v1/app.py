e09ddb5c29161dfbbf3728fabe978a4c6ec45c83
20Emi <iop.emilysanchez1@gmail.com>
2023-08-27 10:28:00 -0700

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,25 +3,33 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
+from os import getenv
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def teardown(exeption):
+    """This def terdadown the database"""
     storage.close()
 
 
 @app.errorhandler(404)
-def Not_found(error):
-    """Manafe error 404"""
-
-    error = {
-        "error": "Not found"
-    }
-    return jsonify(error), 404
+def say_error(error):
+    """ Return error 404"""
+    return jsonify(error="Not found"), 404
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', threaded=True)
+if __name__ == "__main__":
+    if getenv("HBNB_API_HOST"):
+        host = getenv("HBNB_API_HOST")
+    else:
+        host = "0.0.0.0"
+    if getenv("HBNB_API_PORT"):
+        port = getenv("HBNB_API_PORT")
+    else:
+        port = 5000
+    app.run(host=host, port=port, threaded=True)
